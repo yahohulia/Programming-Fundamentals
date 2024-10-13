3b2d56b30fc3a5f2362c82bd307864388ce3d179
yahohulia <yaroslavhohulia@gmail.com>
2024-10-13 12:36:44 +0300

    Update Source.cpp

--- labs/Lab_2.2/Source.cpp
+++ labs/Lab_2.2/Source.cpp
@@ -33,38 +33,3 @@ int main() {
 
 	return 0;
 }
-	// Task 2
-enum Names { Andriy, Dmytro, Oksana, Anastasia, Oleksandr, Kateryna};
-struct Student
-{
-	Names name;
-	float yearofstudy;
-	bool stipend;
-};
-
-int main() {
-
-
-	Student myStudents[7];
-	myStudents[0] = { Anastasia, 2, true };
-	myStudents[1] = { Oleksandr, 2, true };
-	myStudents[2] = { Kateryna, 4, false };
-	myStudents[3] = { Dmytro, 2, false };
-	myStudents[4] = { Andriy, 1, true };
-	myStudents[5] = { Oksana, 3, true };
-	myStudents[6].name = Andriy;
-	myStudents[6].yearofstudy = 2;
-	myStudents[6].stipend = false;
-
-	int count = 0;
-	for (int i = 0; i < 7; i++)
-	{
-		if (myStudents[i].yearofstudy == 2 && myStudents[i].stipend == false)
-		{
-			count++;
-		}
-	}
-	float percent = (float)count * 100 / 7;
-
-	return 0;
-}
